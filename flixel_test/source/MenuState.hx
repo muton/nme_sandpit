517ded90865f2afe419248f506c50554c6e1f523
timh <tim@muton.co.uk>
2013-04-12 13:43:20 +0100

    Figured out haxe json approach and liked it.

--- flixel_test/source/MenuState.hx
+++ flixel_test/source/MenuState.hx
@@ -1,5 +1,6 @@
 package;
 
+import haxe.Json;
 import nme.display.BitmapData;
 import nme.Assets;
 import nme.geom.Rectangle;
@@ -15,6 +16,18 @@ import org.flixel.FlxState;
 import org.flixel.FlxText;
 import org.flixel.FlxU;
 import org.flixel.plugin.photonstorm.FlxDisplay;
+
+typedef DVRConf = {
+	enemies:Array<EnemyInfo>
+}
+
+typedef EnemyInfo = {
+	id:String,
+	mass:Int,
+	validFormations:Array<String>,
+	?optionalExtra:String
+}
+
 
 class MenuState extends FlxState
 {
@@ -38,16 +51,50 @@ class MenuState extends FlxState
 		add( new Dragon() );
 		
 		var emitter = new FlxEmitter( 100, 50, 30 );
-		var colours:Array<UInt> = [ 0xFFFFFF00, 0xFFFFFF00, 0xFFFF8000, 0xFFFF8000, 0xFF00FFFF ];
+		var colours:Array<Int> = [ 0xFFFFFF00, 0xFFFFFF00, 0xFFFF8000, 0xFFFF8000, 0xFF00FFFF ];
 		var bmpd = new BitmapData( colours.length, 1, false );
 		for ( i in 0...colours.length ) {
 			bmpd.setPixel( i, 0, colours[i] );
 		}
 		emitter.makeParticles( bmpd, 50, 0, true );
 		emitter.start( false, 1, 0.01 );
-		
-		
 		add( emitter );
+		
+		testJson();
+	}
+	
+	
+	private function testJson() {
+		
+		var teststr:String = '
+			{
+				"enemies": [
+					{
+						"id": "boringRobot",
+						"mass": 10,
+						"validFormations": ["sin", "zigzag"]
+					},
+					{
+						"id": "killerRobot",
+						"mass": 40,
+						"validFormations": ["sin", "circle"]
+					}
+				]
+			}
+		';
+		
+		var json:Dynamic = Json.parse( teststr );
+		
+		trace( "json: " + json );		
+		
+		trace( "json.enemies.boringRobot: " + json.enemies[0] );
+		
+		var conf:DVRConf = json;
+		
+		trace( "conf: " + conf );
+		
+		var einf:EnemyInfo = conf.enemies[0];
+		trace( "einf ID: " + einf.id );
 	}
 	
 	private function onStartClick() 
