6a635b73676e98022b4a96f3567bf09b1fd44b5f
timh <tim@muton.co.uk>
2013-04-16 12:15:55 +0100

    Figured out how loadRotatedGraphic can (and can't) be used.

--- flixel_test/source/AlienBlob.hx
+++ flixel_test/source/AlienBlob.hx
@@ -1,5 +1,9 @@
 package ;
+import org.flixel.FlxG;
 import org.flixel.FlxSprite;
+import org.flixel.tweens.FlxTween;
+import org.flixel.tweens.motion.LinearMotion;
+import org.flixel.tweens.util.Ease;
 
 /**
  * ...
@@ -13,10 +17,36 @@ class AlienBlob extends FlxSprite {
 		exists = true;									// exists bool tells engine whether to render or not
 		
 		loadGraphic( "assets/alienblob_sheet.png", true, false, 30, 30 );
+		//loadRotatedGraphic( "assets/alienblob_sheet.png", 16, 3, false, false );
 		addAnimation( "basic", [0, 1, 2, 1], 10, true );
+		//addAnimation( "dead", [3], 1 );
 		
 		play( "basic" );
 		
+	}
+	
+	public function playDead() {
+		loadRotatedGraphic( "assets/alienblob_sheet.png", 45, 3, false, false );
+		
+		var m = new LinearMotion( null, FlxTween.ONESHOT );
+		m.setObject( this );
+		m.setMotion( x, y, x + 30, FlxG.height - 10, 1 );
+		//m.start();
+		addTween( m, true );
+		angularVelocity = 120;
+	}
+	
+	override public function update():Void {
+		super.update();
+		
+		var m = FlxG.mouse;
+		
+		if ( m.pressed()
+			&& m.x > x && m.x < x + width
+			&& m.y > y && m.y < y + height ) {
+				
+			playDead();
+		}
 	}
 	
 }
