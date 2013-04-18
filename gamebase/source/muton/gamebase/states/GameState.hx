b5a1925d30e09f038433624eee6fb04cfcc5bab1
timh <tim@muton.co.uk>
2013-04-18 14:20:27 +0100

    Actually managed some interesting lighting using tilemaps and rays.

--- gamebase/source/muton/gamebase/states/GameState.hx
+++ gamebase/source/muton/gamebase/states/GameState.hx
@@ -30,10 +30,16 @@ import org.flixel.plugin.photonstorm.FlxDisplay;
 class GameState extends FlxState {
 	
 	private var bg:FlxSprite;
+	private var floor:FlxTilemap;
 	private var map:FlxTilemap;
+	private var darkness:FlxSprite;
+	private var lighting:FlxSprite;
 	
 	private var lightSources:Array<FlxPoint>;
 	private var player:Player;
+	
+	private var lastFloorTileX:Int;
+	private var lastFloorTileY:Int;
 	
 	override public function create():Void {
 		super.create();
@@ -46,22 +52,49 @@ class GameState extends FlxState {
 		bg.active = false;
 		add( bg );
 		
+		
 		map = new FlxTilemap();
 		map.loadMap( 
 			Assets.getText( "assets/pathfinding/pathfinding_map.txt" ), 
-			Assets.getBitmapData( "assets/tiles/autotiles_16x16.png" ),
+			Assets.getBitmapData( "assets/tiles/autotiles_dark_16x16.png" ),
 			16, 16, FlxTilemap.ALT );
+			
+		floor = new FlxTilemap();
+		floor.drawDebug();
+		floor.widthInTiles = map.widthInTiles;
+		floor.heightInTiles = map.heightInTiles;
+		var blankArr = new Array<Int>();
+		for ( i in 0...map.totalTiles ) { blankArr.push( 0 ); }
+		floor.loadMap(
+			blankArr, 
+			Lighting.genLightMapTileSet( 10, 16, 16, 0.8 ),
+			16, 16, FlxTilemap.OFF, 0, 1, 0 );
+			
+		add( floor );
 		add( map );
 		
-	
 		player = new Player( 20, 20 );
 		add( player );
+		
+		darkness = new FlxSprite( 0, 0 );
+		darkness.makeGraphic( FlxG.width, FlxG.height, 0xFF000000 );
+		darkness.scrollFactor.x = darkness.scrollFactor.y = 0;
+		darkness.blend = BlendMode.MULTIPLY;
+		darkness.active = false;
+		//add( darkness );
+		
+		lighting = new FlxSprite( 0, 0 );
+		lighting.makeGraphic( 140, 140, 0xff000000 );
+		lighting.pixels.draw( Lighting.circSprite( 70 ) );
+		lighting.blend = BlendMode.SCREEN;
 		
 		FlxG.camera.setBounds( 0, 0, map.width, map.height, true );
 		FlxG.camera.follow( player, FlxCamera.STYLE_TOPDOWN, null, 3 );
 		
-		Lighting.redrawBg( bg, lightSources, map.width, map.height );
+		bg.makeGraphic( Std.int( map.width ), Std.int( map.height ), 0xFF000000 );
+		//Lighting.redrawBg( bg, lightSources, map.width, map.height );
 	}
+	
 	
 	override public function destroy():Void {
 		super.destroy();
@@ -69,8 +102,39 @@ class GameState extends FlxState {
 
 	override public function update():Void {
 		FlxG.collide( player, map );
+		var screenXY = player.getScreenXY();
+		darkness.makeGraphic( FlxG.width, FlxG.height, 0xff000000, true );
+		darkness.stamp( lighting, Std.int( screenXY.x - 45 ), Std.int( screenXY.y - 45 ) );
 		super.update();
+		
+		floor.setTile( Std.int( player.x / 16 ), Std.int( player.y / 16 ), 9, true );
+		
+		updateFloorLighting();
 	}	
+		
+	private function updateFloorLighting():Void {
+		var curTileX = Std.int( player.x / 16 );
+		var curTileY = Std.int( player.y / 16 );
+		
+		if ( lastFloorTileX == curTileX && lastFloorTileY == curTileY ) { 
+			return;
+		}
+		
+		lastFloorTileX = curTileX;
+		lastFloorTileY = curTileY;
+		
+		for ( x in Std.int( Math.max( curTileX - 11, 0 ) )...Std.int( Math.min( curTileX + 12, floor.widthInTiles  ) ) ) {
+			for ( y in Std.int( Math.max( curTileY - 11, 0 ) )...Std.int( Math.min( curTileY + 12, floor.heightInTiles ) ) ) {
+				var dist = Math.sqrt( Math.pow( curTileX - x, 2 ) + Math.pow( curTileY - y, 2 ) );
+				var tileNum = 9 - Std.int( Math.min( 9, dist ) );
+				//trace( Std.format( 'x:$x y:$y dist:$dist tileNum:$tileNum' ) );
+				floor.setTile( x, y, 
+					map.ray( new FlxPoint( player.x, player.y ), new FlxPoint( x * 16, y * 16 ), null, 10 ) ? tileNum : 0, true );
+			}
+		}
+	
+	}
+	
 	
 	
 }
--- gamebase/source/muton/gamebase/util/Lighting.hx
+++ gamebase/source/muton/gamebase/util/Lighting.hx
@@ -18,6 +18,7 @@ import org.flixel.FlxSprite;
 class Lighting {
 
 	private static var bgFill:BitmapData;
+	private static var circSpriteCache:IntHash<Sprite>;
 
 	public static inline function redrawBg( bg:FlxSprite, lightSources:Array<FlxPoint>, width:Float, height:Float ) {
 		var flxScale = FlxG.camera.zoom;
@@ -51,12 +52,7 @@ class Lighting {
 			}
 		}
 		
-		var mtr = new Matrix();
-		mtr.createGradientBox( radius * 2, radius * 2, 0, 0, 0 );
-		var circ = new Sprite();
-		circ.graphics.beginGradientFill( GradientType.RADIAL, [0xFFFF00, 0xFF8000], [0.7, 0], [0, 255], mtr );
-		circ.graphics.drawCircle( radius, radius, radius );
-		circ.graphics.endFill();
+		var circ = circSprite( Std.int( radius ) );
 		var pm = new Matrix();
 		
 		for ( pt in lightSources ) {
@@ -64,6 +60,37 @@ class Lighting {
 			bmpd.draw( circ, pm, null, BlendMode.LIGHTEN );
 		}
 		bmpd.unlock();
+	}
+	
+	public static inline function updateLighting( darkness:FlxSprite, movingLS:FlxSprite, lightSources:Array<FlxPoint> ) {
+		
+		//var screenXY = player.getScreenXY();
+		//darkness.makeGraphic( FlxG.width, FlxG.height, 0xff000000, true );
+		//darkness.stamp( lighting, Std.int( player.x - 45 ), Std.int( player.y - 45 ) );
 		
 	}
+	
+	public static inline function circSprite( radius:Int ):Sprite {
+		var mtr = new Matrix();
+		mtr.createGradientBox( radius * 2, radius * 2, 0, 0, 0 );
+		var circ = new Sprite();
+		circ.graphics.beginGradientFill( GradientType.RADIAL, [0xFFFF00, 0xFF8000], [0.7, 0], [0, 255], mtr );
+		circ.graphics.drawCircle( radius, radius, radius );
+		circ.graphics.endFill();
+		return circ;
+	}
+	
+	
+	public static inline function genLightMapTileSet( numTiles:Int, tileWidth:Int, tileHeight:Int, maxAlpha:Float ):BitmapData {
+		var bmpd = new BitmapData( numTiles * tileWidth, tileHeight, true, 0x00000000 );
+		var alphaIncrement = maxAlpha / numTiles * 0xff;
+		var rect:Rectangle = new Rectangle( 0, 0, tileWidth, tileHeight );
+		var baseColour = 0x00FFFFB9;
+		for ( i in 0...numTiles ) {
+			rect.x = i * tileWidth;
+			bmpd.fillRect( rect, baseColour + ( Std.int( alphaIncrement * i ) << 24 ) );
+		}
+		return bmpd;
+	}
+	
 }
